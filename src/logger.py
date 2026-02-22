45b301a2d04d8c54bed38962d0d1295ab98ad3b7
sivadath <sivadaths963@gmail.com>
2026-02-22 20:12:05 +0530

    EDA and problem statement

--- src/logger.py
+++ src/logger.py
@@ -17,7 +17,9 @@ logging.basicConfig(
 
 if __name__=="__main__":
     logging.info("Logging has started")
-    '''
+ 
+       '''
+'''
 import logging
 import os
 from datetime import datetime
@@ -40,3 +42,31 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+'''
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+logs_folder = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_folder, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_folder, LOG_FILE)
+
+# Create logger
+logger = logging.getLogger("my_logger")
+logger.setLevel(logging.INFO)
+
+# Create file handler
+file_handler = logging.FileHandler(LOG_FILE_PATH)
+file_handler.setLevel(logging.INFO)
+
+# Create formatter
+formatter = logging.Formatter(
+    "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+)
+file_handler.setFormatter(formatter)
+
+# Add handler to logger
+logger.addHandler(file_handler)
